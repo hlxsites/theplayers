b8df31fe211c43d6ccf3c3d0ca8ff4b09baa7b94
Sean Steimer <ssteimer@adobe.com>
2022-10-03 11:35:43 -0700

    committing here, more work to do

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -179,58 +179,71 @@ export default async function decorate(block) {
           // setup overview (title, img, desc)
           const overview = document.createElement('div');
           overview.classList.add('course-overview');
-          overview.append(
-            text.querySelector('h2'), // title
-            text.querySelector('h2 + h3'), // par heading
-            text.querySelector('h2 + h3 + p'), // course img
-            text.querySelector('h2 + h3 + p + p'), // course desc
-          );
+
+          const title = text.querySelector('h2'); // hole #
+          const par = text.querySelector('h2 + h3');
+          const holeName = text.querySelector('h2 + h3 + h3');
+          const courseImg = text.querySelector(holeName ? 'h2 + h3 + h3 + p' : 'h2 + h3 + p');
+          const holeDesc = text.querySelector(holeName ? 'h2 + h3 + h3 + p + p' : 'h2 + h3 + p + p');
+
+          overview.append(title, par);
+          overview.append(holeName ? holeName : '<h3></h3>');
+          overview.append(courseImg ? courseImg : '<p></p>');
+          overview.append(holeDesc);
+
           const holeImg = overview.querySelector('picture');
           if (holeImg) holeImg.parentNode.classList.add('course-hole');
           // setup stats
-          const statistics = document.createElement('div');
-          statistics.classList.add('course-statistics');
-          statistics.append(
-            text.querySelector('h3'), // statistics heading
-            text.querySelector('h3 + ul'), // statistics list
-          );
-          const statsTable = document.createElement('table');
-          const allStats = statistics.querySelector('ul');
-          let stats = [];
-          if (allStats) stats = allStats.querySelectorAll('li');
-          stats.forEach((s) => {
-            const stat = s.querySelector('strong').textContent;
-            // setup scoring average ring
-            if (stat.toUpperCase() === 'SCORING AVG') {
-              const avg = document.createElement('div');
-              avg.classList.add('course-avg');
-              avg.innerHTML = `<p>${s.innerHTML}</p>`;
-              allStats.parentNode.insertBefore(avg, allStats);
-              s.remove();
-            } else {
-              const tableRow = document.createElement('tr');
-              tableRow.classList.add(`course-${toClassName(stat)}`);
-              const val = parseInt(s.textContent.split(' ')[s.textContent.split(' ').length - 1], 10);
+          const statsHeading = text.querySelector('h3');
+          if (statsHeading) {
+            const statistics = document.createElement('div');
+            statistics.classList.add('course-statistics');
+            statistics.append(
+              statsHeading, // statistics heading
+              text.querySelector('h3 + ul'), // statistics list
+            );
+            const statsTable = document.createElement('table');
+            const allStats = statistics.querySelector('ul');
+            let stats = [];
+            if (allStats) stats = allStats.querySelectorAll('li');
+            stats.forEach((s) => {
+              const stat = s.querySelector('strong').textContent;
+              // setup scoring average ring
+              if (stat.toUpperCase() === 'SCORING AVG') {
+                const avg = document.createElement('div');
+                avg.classList.add('course-avg');
+                avg.innerHTML = `<p>${s.innerHTML}</p>`;
+                allStats.parentNode.insertBefore(avg, allStats);
+                s.remove();
+              } else {
+                const tableRow = document.createElement('tr');
+                tableRow.classList.add(`course-${toClassName(stat)}`);
+                const val = parseInt(s.textContent.split(' ')[s.textContent.split(' ').length - 1], 10);
 
-              const bar = document.createElement('td');
-              bar.classList.add('course-stat-graph');
-              bar.innerHTML = `<div class="course-stat-bar" style="width: ${val}%"></div>`;
+                const bar = document.createElement('td');
+                bar.classList.add('course-stat-graph');
+                bar.innerHTML = `<div class="course-stat-bar" style="width: ${val}%"></div>`;
 
-              const percent = document.createElement('td');
-              percent.classList.add('course-stat-percent');
-              percent.innerHTML = `${val}%`;
+                const percent = document.createElement('td');
+                percent.classList.add('course-stat-percent');
+                percent.innerHTML = `${val}%`;
 
-              const thisStat = document.createElement('td');
-              thisStat.classList.add('course-stat-title');
-              thisStat.innerHTML = s.querySelector('strong').textContent;
+                const thisStat = document.createElement('td');
+                thisStat.classList.add('course-stat-title');
+                thisStat.innerHTML = s.querySelector('strong').textContent;
 
-              tableRow.append(bar, percent, thisStat);
-              statsTable.append(tableRow);
-            }
-          });
-          if (allStats && statsTable) allStats.replaceWith(statsTable);
+                tableRow.append(bar, percent, thisStat);
+                statsTable.append(tableRow);
+              }
+            });
 
-          text.prepend(overview, statistics);
+            if (allStats && statsTable) allStats.replaceWith(statsTable);
+
+            text.prepend(overview, statistics);
+          } else {
+            text.prepend(overview);
+          }
+
           // setup photo credits
           const credits = text.querySelector('p > em');
           if (credits) credits.parentNode.classList.add('course-credits');
