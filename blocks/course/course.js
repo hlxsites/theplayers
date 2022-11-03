52e39057489c8f0e0de36b438c3f03410c791739
Katie Sexton <ksexton@adobe.com>
2022-11-03 14:36:44 -0600

    feat: new course block

--- blocks/course/course.js
+++ blocks/course/course.js
@@ -1,4 +1,4 @@
-import { readBlockConfig, fetchPlaceholders, toClassName } from '../../scripts/scripts.js';
+import { fetchPlaceholders } from '../../scripts/scripts.js';
 
 function findStatPercent(id, stats, divisor) {
   const stat = stats.find((s) => s.id === id);
@@ -11,7 +11,6 @@ function findStatPercent(id, stats, divisor) {
 
 function buildStatRow(stat, value) {
   const row = document.createElement('tr');
-  row.className = `course-hole-${toClassName(stat)}`;
   row.innerHTML = `<td>
       <div class="course-hole-bar" style="width: ${value}%"></div>
     </td>
@@ -20,87 +19,109 @@ function buildStatRow(stat, value) {
   return row;
 }
 
+async function loadStats(block) {
+  const placeholders = await fetchPlaceholders();
+  const code = placeholders.tourCode;
+  const id = placeholders.tournamentId;
+  const resp = await fetch(`https://little-forest-58aa.david8603.workers.dev/?url=${encodeURIComponent(`https://statdata.pgatour.com/${code}/${id}/coursestat.json`)}`);
+  const json = await resp.json();
+  if (json && json.courses && json.courses[0].holes) {
+    const holes = block.querySelectorAll('.course-hole');
+    const allStats = json.courses[0].holes;
+    holes.forEach((hole, i) => {
+      const stats = allStats[i];
+      const par = hole.querySelector('.course-hole-par');
+      if (par && stats.par) par.textContent = `Par ${stats.par},`;
+      const yards = hole.querySelector('.course-hole-yards');
+      if (yards && stats.yards) yards.textContent = `${stats.yards} Yards`;
+      const title = hole.querySelector('.course-hole-stats h3');
+      if (title) title.textContent = `${json.year || ''} Statistics`.trim();
+      const avgEl = hole.querySelector('.course-hole-avg');
+      const avg = stats.stats.find((stat) => stat.id === '43108').eV2;
+      if (avgEl && avg) avgEl.innerHTML = `<span>${avg}</span> Scoring Avg`;
+      const table = hole.querySelector('.course-hole-chart tbody');
+      if (table) {
+        const holeStats = stats.stats.filter((stat) => stat.id !== '43108');
+        const statsDivisor = holeStats.reduce((a, b) => {
+          // eslint-disable-next-line no-param-reassign
+          if (a.eV2) a = a.eV2;
+          return parseInt(a, 10) + parseInt(b.eV2, 10);
+        });
+        const tableStats = [
+          { stat: 'Eagle', value: findStatPercent('43106', holeStats, statsDivisor) },
+          { stat: 'Birdie', value: findStatPercent('43107', holeStats, statsDivisor) },
+          { stat: 'Par', value: findStatPercent('43523', holeStats, statsDivisor) },
+          { stat: 'Bogey', value: findStatPercent('41184', holeStats, statsDivisor) },
+          { stat: '2+ Bogey', value: findStatPercent('43520', holeStats, statsDivisor) },
+        ];
+        tableStats.forEach((s) => table.append(buildStatRow(s.stat, s.value)));
+      }
+    });
+  }
+}
+
 export default async function decorate(block) {
-  const config = readBlockConfig(block);
-  block.innerHTML = '';
+  block.parentElement.style.visibility = 'hidden';
   const observer = new IntersectionObserver(async (entries) => {
     if (entries.some((entry) => entry.isIntersecting)) {
-      const placeholders = await fetchPlaceholders();
-      const code = placeholders.tourCode;
-      const id = placeholders.tournamentId;
-      const statsResp = await fetch(`https://little-forest-58aa.david8603.workers.dev/?url=${encodeURIComponent(`https://statdata.pgatour.com/${code}/${id}/coursestat.json`)}`);
-      const statsJson = await statsResp.json();
-      const courseResp = await fetch('/course.json');
-      const courseJson = await courseResp.json();
-      if (
-        courseJson && courseJson.data
-        && statsJson && statsJson.courses && statsJson.courses[0].holes
-      ) {
-        const buttons = document.createElement('div');
-        buttons.className = 'button-container';
-        // block.parentElement.prepend(buttons);
-        const courseData = courseJson.data;
-        const courseStats = statsJson.courses[0].holes;
-        if (courseData.length === courseStats.length) {
-          courseData.forEach((data, i) => {
-            const stats = courseStats[i];
-            // setup button
-            const button = document.createElement('button');
-            if (!i) button.classList.add('selected');
-            buttons.append(button);
-            // setup hole slide
-            const hole = document.createElement('div');
-            hole.className = 'course-hole';
-            const holeImg = document.createElement('div');
-            holeImg.className = 'course-hole-image';
-            holeImg.innerHTML = `<picture><img src="${data.image}" alt="${data.title}" /></picture>`;
-            const holeText = document.createElement('div');
-            holeText.className = 'course-hole-text';
-            holeText.innerHTML = `<div class="course-hole-data">
-                <h2 class="course-hole-title">Hole #${data.hole || i + 1}</h2>
-                <p class="course-hole-subtitle">
-                  ${stats.par ? `<span class="course-hole-par">Par ${stats.par}</span>` : ''}, 
-                  ${stats.yards ? `<span class="course-hole-yards">${stats.yards} Yards</span>` : ''}
-                </p>
-                ${data.overheadImage ? `<img class="course-hole-overhead" src="${data.overheadImage}" alt="${data.title}" />` : ''}
-                <p>${data.description}</p>
+      const hasStats = block.className.includes('stats');
+      const buttons = document.createElement('div');
+      buttons.className = 'course-buttons';
+      [...block.children].forEach((row, i) => {
+        row.className = `course-hole course-hole-${i + 1}`;
+        const classes = ['image', 'text'];
+        classes.forEach((c, j) => {
+          if (row.children[j]) row.children[j].classList.add(`course-hole-${c}`);
+        });
+        const text = row.querySelector('.course-hole-text');
+        if (text) {
+          const credits = document.createElement('div');
+          credits.className = 'course-hole-credits';
+          credits.append(text.lastElementChild);
+          const data = document.createElement('div');
+          data.className = 'course-hole-data';
+          [...text.children].forEach((child) => data.append(child));
+          const title = data.querySelector('h2');
+          title.className = 'course-hole-title';
+          const subtitle = document.createElement('p');
+          subtitle.className = 'course-hole-subtitle';
+          subtitle.innerHTML = `<span class="course-hole-par">&nbsp;</span> 
+            <span class="course-hole-yards">&nbsp;</span>`;
+          title.after(subtitle);
+          const overhead = data.querySelector('picture > img');
+          if (overhead) overhead.parentElement.parentElement.className = 'course-hole-overhead';
+          text.append(data, credits);
+          // prebuild stats in stats variant
+          if (hasStats) {
+            const stats = document.createElement('div');
+            stats.className = 'course-hole-stats';
+            stats.innerHTML = `<h3>&nbsp;</h3>
+              <div class="course-hole-avg">
+                <p></p>
               </div>
-              <div class="course-hole-credits">
-                <p>Photo by <strong>${data.creator}</strong> / ${data.rights}</p>
-              </div>`;
-            if (config.stats === statsJson.year) { // build stats block
-              const avg = stats.stats.find((stat) => stat.id === '43108').eV2;
-              const allStats = stats.stats.filter((stat) => stat.id !== '43108');
-              const statsDivisor = allStats.reduce((a, b) => {
-                // eslint-disable-next-line no-param-reassign
-                if (a.eV2) a = a.eV2;
-                return parseInt(a, 10) + parseInt(b.eV2, 10);
-              });
-              const tableStats = [
-                { stat: 'Eagle', value: findStatPercent('43106', allStats, statsDivisor) },
-                { stat: 'Birdie', value: findStatPercent('43107', allStats, statsDivisor) },
-                { stat: 'Par', value: findStatPercent('43523', allStats, statsDivisor) },
-                { stat: 'Bogey', value: findStatPercent('41184', allStats, statsDivisor) },
-                { stat: '2+ Bogey', value: findStatPercent('43520', allStats, statsDivisor) },
-              ];
-              const table = document.createElement('tbody');
-              tableStats.forEach((s) => table.append(buildStatRow(s.stat, s.value)));
-              const statsEl = document.createElement('div');
-              statsEl.className = 'course-hole-stats';
-              statsEl.innerHTML = `<h3>${config.stats} Statistics</h3>
-                <div class="course-hole-avg">
-                  <p><span>${avg}</span> Scoring Avg</p>
-                </div>
-                <table class="course-hole-chart"><tbody>
-                  ${table.outerHTML}
-                </tbody></table>`;
-              holeText.append(statsEl);
-            }
-            hole.append(holeImg, holeText);
-            block.append(hole);
-          });
+              <table class="course-hole-chart"><tbody></tbody></table>`;
+            text.append(stats);
+          }
         }
-      }
+        // add button
+        const button = document.createElement('button');
+        if (!i) {
+          button.classList.add('selected');
+          buttons.setAttribute('aria-hidden', true);
+        } else {
+          buttons.removeAttribute('aria-hidden');
+        }
+        button.addEventListener('click', () => {
+          block.scrollTo({ top: 0, left: row.offsetLeft - row.parentNode.offsetLeft, behavior: 'smooth' });
+          [...buttons.children].forEach((r) => r.classList.remove('selected'));
+          button.classList.add('selected');
+        });
+        buttons.append(button);
+      });
+      if (buttons.hasChildNodes()) block.parentElement.prepend(buttons);
+      block.parentElement.removeAttribute('style');
+      // populate stats in stats variant
+      if (hasStats) loadStats(block);
     }
   }, { threshold: 0 });
 
