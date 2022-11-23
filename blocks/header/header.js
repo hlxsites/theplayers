afa90540cc56270faf8aaeef0fba10b77ba1cf18
Sean Steimer <ssteimer@adobe.com>
2022-11-23 12:59:16 -0800

    feat: header for mobile. This is still a bit WIP, but getting closer to fully functional

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -40,51 +40,97 @@ export default async function decorate(block) {
       // tours dropdown
       nav.append(syntheticHeader.querySelector('.other-tours-dropdown'));
       const toursNav = nav.querySelector('.other-tours-dropdown .header-tours-nav');
-      toursNav.style.display = 'none';
       nav.append(toursNav);
 
       const toursButton = nav.querySelector('.other-tours-dropdown .other-tours');
       toursButton.addEventListener('click', () => {
-        const expanded = nav.getAttribute('aria-expanded') === 'true';
-        toursNav.style.display = expanded ? 'none' : 'block';
+        const expanded = toursButton.classList.contains('active');
+        toursNav.classList.toggle('expanded');
         nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+        document.body.style.overflowY = expanded ? '' : 'hidden';
+        nav.classList.remove('search-active');
         toursButton.classList.toggle('active')
       });
 
-      const arrow = document.createElement('div');
-      arrow.innerText = '>';
-      arrow.classList.add('arrow')
-      toursButton.appendChild(arrow);
-
       // search
-      const search = syntheticHeader.querySelector('.nav-search');
+      const search = document.createElement('div');
+      search.classList.add('nav-search')
+
+      const searchForm = document.createElement('div');
+      const searchButton = document.createElement('span');
+      searchButton.classList.add('search-icon');
+      searchButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        nav.classList.toggle('search-active');
+      });
+      search.append(searchButton);
+
+      
+      searchForm.classList.add('search-form');
+      const searchText = document.createElement('input');
+      searchText.placeholder = 'Search...';
+      searchForm.append(searchText);
+      
       nav.append(search);
+      nav.append(searchForm);
 
       // super nav dropdown
       nav.append(syntheticHeader.querySelector('.fatNavigation2 .header-subnav'));
       const subNav = nav.querySelector('.header-subnav');
-      subNav.style.display = 'none';
-
-      // nav links
-      // nav.append(syntheticHeader.querySelector('.nav'));
-
-      // login
+      subNav.querySelectorAll('.drillDownMenu > li').forEach((listItem) => {
+        const ul = listItem.querySelector('ul');
+        if (ul) {
+          listItem.classList.add('hasSubs');
+          let prevText = '';
+          listItem.querySelector('a').addEventListener('click', (e) => {
+            if (prevText === '') {
+              prevText = e.target.innerText;
+              e.target.innerText = 'back';
+            } else {
+              e.target.innerText = prevText;
+              prevText = '';
+            }
+            
+            e.preventDefault();
+            ul.classList.toggle('active');
+            listItem.classList.toggle('active');
+            listItem.closest('ul').classList.toggle('sub-active');
+          });
+        }
+      });
 
       // hamburger
       const hamburger = document.createElement('div');
       hamburger.classList.add('nav-hamburger');
       hamburger.innerHTML = '<div class="nav-hamburger-icon"></div>';
       hamburger.addEventListener('click', () => {
-        const expanded = nav.getAttribute('aria-expanded') === 'true';
+        const expanded = hamburger.classList.contains('active');
+        const toursExpanded = toursButton.classList.contains('active');
+        if(toursExpanded) {
+          toursNav.classList.toggle('expanded');
+          toursButton.classList.toggle('active')
+        }
+
         nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-        subNav.style.display = expanded ? 'none' : 'block';
+        document.body.style.overflowY = expanded ? '' : 'hidden';
+        subNav.classList.toggle('expanded');
         toursButton.style.display = expanded ? 'block' : 'none';
         search.style.display = expanded ? 'block' : 'none';
+
+        hamburger.classList.toggle('active')
+        nav.classList.remove('search-active');
       });
       nav.prepend(hamburger);
       nav.setAttribute('aria-expanded', 'false');
 
       // leaderboard
+
+      // nav links
+      // nav.append(syntheticHeader.querySelector('.nav'));
+
+      // login
+
+      // watch now links
 
     }
 
