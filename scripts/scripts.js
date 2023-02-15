3c8178254b0046b2cf9e1920ed5ee445ba435ebe
David Nuescheler <david@nuescheler.ch>
2023-02-15 09:21:17 -0700

    feat: add inp to performance lib

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1112,3 +1112,12 @@ try {
     },
   });
 } catch (e) { /* ignore */ }
+
+
+const params = new URLSearchParams(window.location.search);
+if (params.get('performance')) {
+  window.hlx.performance = true;
+  import('./lib-franklin-performance.js').then((mod) => {
+    if (mod.default) mod.default();
+  });
+}
