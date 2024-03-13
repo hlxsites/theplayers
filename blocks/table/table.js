ca609872d8f44996b4a2a68689b8481af845fd68
Mark McConnell <markm@ensemble.com>
2024-03-13 15:05:01 -0400

    - Updated Data Table to leverage MW feed

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -1,4 +1,4 @@
-import { readBlockConfig } from '../../scripts/scripts.js';
+import { fetchGraphQL, fetchPlaceholders, readBlockConfig } from '../../scripts/scripts.js';
 
 function buildCell(col, rowNum) {
   const cell = rowNum > 0 ? document.createElement('td') : document.createElement('th');
@@ -13,48 +13,53 @@ function buildCell(col, rowNum) {
   return cell;
 }
 
-async function buildDataTable(table, head, body, src, config) {
-  const resp = await fetch(src);
-  if (resp.ok) {
-    const { tours } = await resp.json();
-    if (
-      tours && tours[0]
-      && tours[0].years && tours[0].years && tours[0].years[0]
-      && tours[0].years[0].stats && tours[0].years[0].stats[0]
-    ) {
-      const stats = tours[0].years[0].stats[0];
+async function buildDataTable(table, head, body, resp, config) {
+  if (resp.statDetails) {
+    const { statDetails } = resp;
+    if (statDetails.rows && statDetails.statHeaders) {
       // build table head
       const headData = [
         'Rank This Week',
-        'Rank Last Week',
+        'Rank Difference',
         'Player Name',
-        stats.rndOrEvt,
-        ...Object.values(stats.statTitles).slice(1),
+        ...Object.values(statDetails.statHeaders),
       ];
       headData.forEach((hd) => head.append(buildCell(hd)));
       // build table body
       for (let i = 0; i < 5; i += 1) { // display top five
-        const player = stats.details[i];
-        const playerLink = document.createElement('p');
+        const player = statDetails.rows[i];
+        const playerName = player.playerName.split(' ');
+        const playerLink = document.createElement('em');
         playerLink.innerHTML = `<a
-          href="https://www.pgatour.com/players/player.${player.plrNum}.${player.plrName.first}-${player.plrName.last}.html">
-            ${player.plrName.first} ${player.plrName.last}
+          href="https://www.pgatour.com/players/player.${player.playerId}.${playerName[0]}-${playerName[1]}.html">
+            ${playerName[0]} ${playerName[1]}
           </a>`;
+        const rankDifferenceContainer = document.createElement('p');
+        const rankDifference = document.createElement('div');
+        rankDifferenceContainer.append(rankDifference);
+        if (player.rankDiff === '0' || player.rankDiff === '-') {
+          rankDifference.innerHTML = '-';
+        } else if (player.rankChangeTendency === 'UP') {
+          rankDifference.innerHTML = `+${player.rankDiff}`;
+          rankDifference.style.color = '#25845D';
+        } else if (player.rankChangeTendency === 'DOWN') {
+          rankDifference.innerHTML = `-${player.rankDiff}`;
+          rankDifference.style.color = '#ED0000';
+        }
         const rowData = [
-          player.curRank,
-          player.prevRank,
+          player.rank,
+          rankDifferenceContainer,
           playerLink,
-          ...Object.values(player.statValues),
-        ];
+          ...Object.values(Object.values(player.stats)).map((stat) => stat.statValue)];
         const row = document.createElement('tr');
         rowData.forEach((rd) => row.append(buildCell(rd, i + 1)));
         body.append(row);
       }
       table.append(head, body);
       // build caption
-      if (stats.statName) {
+      if (statDetails.statTitle) {
         const caption = document.createElement('caption');
-        caption.innerHTML = `${config.year || new Date().getFullYear()} <strong>${stats.statName}</strong>`;
+        caption.innerHTML = `${config.year || new Date().getFullYear()} <strong>${statDetails.statTitle}</strong>`;
         table.prepend(caption);
       }
     }
@@ -62,8 +67,82 @@ async function buildDataTable(table, head, body, src, config) {
   return table;
 }
 
-function buildStatsURL(config) {
-  return `https://statdata.pgatour.com/r/stats/${config.year || new Date().getFullYear}/${config['stat-id']}.json`;
+async function getStatDetails(config) {
+  try {
+    const placeholders = await fetchPlaceholders();
+    const resp = await fetchGraphQL(`query StatDetails($statId: String!, $tourCode: TourCode!, $year: Int, $eventQuery: StatDetailEventQuery) {
+  statDetails(statId: $statId, tourCode: $tourCode, eventQuery: $eventQuery, year: $year) {
+    displaySeason
+    lastProcessed
+    statDescription
+    statHeaders
+    statId
+    statTitle
+    tourCode
+    tourAvg
+    statType
+    tournamentPills {
+      displayName
+      tournamentId
+    }
+    year
+    yearPills {
+      displaySeason
+      year
+    }
+    cutOffNumber
+    cutOffButtonText
+    rows {
+    ... on StatDetailsPlayer {
+        __typename
+        rankChangeTendency
+        country
+        countryFlag
+        playerId
+        playerName
+        rank
+        rankDiff
+        rankLogoDark
+        rankLogoLight
+        stats {
+          color
+          statName
+          statValue
+        }
+      }
+    ... on StatDetailTourAvg {
+        __typename
+        displayName
+        value
+      }
+    }
+    statCategories {
+      category
+      displayName
+      subCategories {
+        stats {
+          statId
+          statTitle
+        }
+        displayName
+      }
+    }
+  }
+}`, {
+      tourCode: placeholders.tourCode.toUpperCase(),
+      statId: config['stat-id'],
+      year: config.year,
+    });
+    if (resp.ok) {
+      const json = await resp.json();
+      if (json.data) {
+        return json.data;
+      }
+    }
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.warn('Could not retrieve Stat Details', err);
+  }
 }
 
 export default async function decorate(block) {
@@ -77,7 +156,13 @@ export default async function decorate(block) {
       const observer = new IntersectionObserver(async (entries) => {
         if (entries.some((entry) => entry.isIntersecting)) {
           observer.disconnect();
-          const statsTable = await buildDataTable(table, head, body, buildStatsURL(config), config);
+          const statsTable = await buildDataTable(
+            table,
+            head,
+            body,
+            await getStatDetails(config),
+            config,
+          );
           block.innerHTML = statsTable.outerHTML;
         }
       }, { threshold: 0 });
