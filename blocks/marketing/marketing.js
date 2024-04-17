4ca9a4840b51f29d137291332b308b17bc0e0cb8
bosco-ensemble <boscok@ensemble.com>
2024-04-17 09:49:25 -0700

    TDW-2409 added ability to fetch ad_test value from url params

--- blocks/marketing/marketing.js
+++ blocks/marketing/marketing.js
@@ -82,10 +82,21 @@ export default async function decorate(block) {
             }
           } else {
             window.tude.setFeatureFlags({ injectAds: false });
-            window.tude.refreshAdsViaDivMappings([{
-              divId: slot,
-              baseDivId: slot,
-            }]);
+            const ad_test = new URLSearchParams(window.location.search).get('ad_test');
+            if(ad_test) {
+              window.tude.refreshAdsViaDivMappings([{
+                divId: slot,
+                baseDivId: slot,
+                targeting: {
+                  ad_test: ad_test
+                }
+              }]);
+            } else {
+              window.tude.refreshAdsViaDivMappings([{
+                divId: slot,
+                baseDivId: slot,
+              }]);
+            }
           }
         });
       });
